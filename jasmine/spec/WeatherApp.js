9bb5b908c16d50c5c5144c3867d2cf4cb8966045
Mathew Taylor <mathew.taylor@vizolution.co.uk>
2018-03-08 16:08:44 +0000

    Fix jasmine tests

--- jasmine/spec/WeatherApp.js
+++ jasmine/spec/WeatherApp.js
@@ -1,13 +1,12 @@
-/* global describe it expect GetCitiesFromStorage */
+/* global beforeEach describe it expect GetCitiesFromStorage RemoveCitiesFromStorage */
 
-describe("Weather App", function() {
-    it("Cities Storage Management", function() {
-        expect(true).toBe(true);
+describe("Weather App - Storage Management", function() {
+    beforeEach(function() {
+        RemoveCitiesFromStorage();
     });
 
     it("Initial storage is correct", function() {
         var expectedCities = "London;Rome;New York;Toronto;Berlin;Dubai";
-
         var returnedCities = GetCitiesFromStorage();
 
         expect(expectedCities === returnedCities).toBe(true);
@@ -16,7 +15,6 @@ describe("Weather App", function() {
     
     it("Initial storage is not correct", function() {
         var expectedCities = "London;Rome;New York;Toronto;Berlin;Dubaixyz";
-
         var returnedCities = GetCitiesFromStorage();
 
         expect(expectedCities === returnedCities).toBe(false);
