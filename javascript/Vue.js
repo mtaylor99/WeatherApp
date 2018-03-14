4766f61717a85db05cadee8aeab9784c6a35066b
Mathew Taylor <mathew.taylor@vizolution.co.uk>
2018-03-14 10:00:11 +0000

    Re-factor

--- javascript/Vue.js
+++ javascript/Vue.js
@@ -1,12 +1,12 @@
 /* global Vue */
 
-Vue.component("todo-item", {
-    // The todo-item component now accepts a "prop", which is like a custom attribute. This prop is called todo.
-    props: ["todo"],
+Vue.component("city-widget", {
+    // The city-widget component now accepts a "prop", which is like a custom attribute. This prop is called city.
+    props: ["city"],
     template: "<div class=\"c-city-widget\">" +
-              "    <button id=\"{{ todo.name }}\" class=\"js-city-list c-city-widget-button\" alt=\"{{ todo.name }}\" tabindex=\"3\">" +
+              "    <button id=\"{{ city.name }}\" class=\"js-city-list c-city-widget-button\" alt=\"{{ city.name }}\" tabindex=\"3\">" +
               "        <div class=\"c-city-widget-city\">" +
-              "            <label>{{ todo.name }}</label>" +
+              "            <label>{{ city.name }}</label>" +
               "        </div>" +
               "        <br/>" +
               "        <div class=\"c-city-widget-weather-icon\">" +
@@ -14,7 +14,7 @@ Vue.component("todo-item", {
               "        </div>" +
               "        <br/>"  +
               "        <div>" +
-              "            <label class=\"c-city-widget-temperature-medium\">{{ todo.temperature }}&#8451</label>" +
+              "            <label class=\"c-city-widget-temperature-medium\">{{ city.temperature }}&#8451</label>" +
               "        </div>" +
               "    </button>" +
               "</div>"
@@ -23,22 +23,19 @@ Vue.component("todo-item", {
 var vueApp = new Vue({
     el: ".js-city-widgets",
     data: {
-        groceryList: [
+        cities: [
             { 
                 id: 0, 
-                text: "Vegetables",
                 name: "London",
                 temperature: 9.6
             },
             { 
                 id: 1, 
-                text: "Cheese",
                 name: "New York",
                 temperature: 9.6
             },
             { 
                 id: 2, 
-                text: "Whatever else humans are supposed to eat",
                 name: "Dublin",
                 temperature: 9.6
             }
